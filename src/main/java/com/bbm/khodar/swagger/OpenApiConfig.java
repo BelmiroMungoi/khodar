16867371eb154847464a2fd299c78753724c5c2c
Belmiro Mungoi <belmiromungoi@gmail.com>
2024-01-24 00:53:49 +0200

    Adding swagger

--- src/main/java/com/bbm/khodar/swagger/OpenApiConfig.java
+++ src/main/java/com/bbm/khodar/swagger/OpenApiConfig.java
@@ -1,5 +1,43 @@
 package com.bbm.khodar.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "KhoEvent",
+                description = "Uma aplicação para a gestão de eventos para as comunidades de Moz",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Belmiro Mungoi",
+                        email = "belmiromungoi@gmail.com",
+                        url = "https://github.com/BelmiroMungoi"
+                ),
+                license = @License(
+                        name = "Apache License 2.0",
+                        url = "https://www.apache.org/licenses"
+                )
+        ),
+        security = {
+                @SecurityRequirement(
+                        name = "Bearer Authentication"
+                )
+        }
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        description = "Autentique-se na API usando as suas credenciais, " +
+                "copie o JWT token e cole no campo abaixo.",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class OpenApiConfig {
 }
